c9504ed914d9b10de96fc08e85905174bade71be
kai-bo <kai.buckhalter@gmail.com>
2023-03-20 03:08:21 -0400

    Vertical alignment property added

--- inheritance/inheritance.lua
+++ inheritance/inheritance.lua
@@ -1,7 +1,9 @@
 align = {
+    top = "top",
     left = "left",
     center = "center",
     right = "right",
+    bottom = "bottom",
 }
 
 Cell = {
@@ -56,7 +58,8 @@ end
 Text = Element:new{
     text = "",
     padding = 0,
-    alignment = align.left,
+    horizontalAlignment = align.left,
+    verticalAlignment = align.top,
     textColor = colors.white,
     transparentBackground = false,
     backgroundColor = colors.black,
@@ -78,46 +81,67 @@ function Text:new(o)
         end
     end
 
+    local textRows = {}
+
     local stringBegin = 1
-    local textWidth = o.width - o.padding * 2
-    local stringEnd = stringBegin + textWidth - 1
+    local rowWidth = o.width - o.padding * 2
+    local stringEnd = stringBegin + rowWidth - 1
     local newLineIndex = o.text:find("\n", stringBegin, true)
     local substring
-    local substringIndex = 1
+    
+    while stringBegin < #o.text do
+        if newLineIndex and newLineIndex < stringBegin then
+            newLineIndex = o.text:find("\n", stringBegin, true)
+        end
+        
+        if newLineIndex and newLineIndex < stringEnd then
+            substring = o.text:sub(stringBegin, newLineIndex)
+            stringBegin = newLineIndex + 1
+        else
+            substring = o.text:sub(stringBegin, stringEnd)
+            stringBegin = stringEnd + 1
+        end
+        
+        stringEnd = stringBegin + rowWidth - 1
+        
+        table.insert(textRows, substring)
+    end
+    
+    local verticalOffset
+    if o.verticalAlignment == align.top then
+        verticalOffset = o.padding
+    elseif o.verticalAlignment == align.center then
+        verticalOffset = o.height / 2 - #textRows / 2
+    elseif o.verticalAlignment == align.bottom then
+        verticalOffset = o.height - o.padding - #textRows
+    end
+    
+    local horizontalOffsets = {}
+    for index, text in ipairs(textRows) do
+        if o.horizontalAlignment == align.left then
+            table.insert(horizontalOffsets, o.padding)
+        elseif o.horizontalAlignment == align.center then
+            table.insert(horizontalOffsets, o.width / 2 - #textRows[index] / 2)
+        elseif o.horizontalAlignment == align.right then
+            table.insert(horizontalOffsets, o.width - o.padding - #textRows[index])
+        end
+    end
 
-    local textOffset
+    local substringIndex = 1
+    local rowIndex = 1
 
     for y = 1, o.height do
-        if y > o.padding and y <= o.height - o.padding then
-            if newLineIndex and newLineIndex < stringBegin then
-                newLineIndex = o.text:find("\n", stringBegin, true)
-            end
-
-            if newLineIndex and newLineIndex < stringEnd then
-                substring = o.text:sub(stringBegin, newLineIndex)
-                stringBegin = newLineIndex + 1
-            else
-                substring = o.text:sub(stringBegin, stringEnd)
-                stringBegin = stringEnd + 1
-            end
-
-            stringEnd = stringBegin + textWidth - 1
+        if y > verticalOffset and y <= verticalOffset + #textRows then
             substringIndex = 1
-
-            if o.alignment == align.left then
-                textOffset = o.padding
-            elseif o.alignment == align.center then
-                textOffset = o.width / 2 - #substring / 2
-            elseif o.alignment == align.right then
-                textOffset = o.width - o.padding - #substring
-            end
-
+            
             for x = 1, o.width do
-                if x > textOffset and x <= textOffset + #substring then
-                    o.cells[x][y].character = substring:sub(substringIndex, substringIndex)
+                if x > horizontalOffsets[rowIndex] and x <= horizontalOffsets[rowIndex] + #textRows[rowIndex] then
+                    o.cells[x][y].character = textRows[rowIndex]:sub(substringIndex, substringIndex)
                     substringIndex = substringIndex + 1
                 end
             end
+
+            rowIndex = rowIndex + 1
         end
     end
 
@@ -125,11 +149,13 @@ function Text:new(o)
 end
 
 text = Text:new{
-    text = "text",
+    text = "abcdefghijklmnopqrstuvwxyz",
+    horizontalAlignment = align.center,
+    verticalAlignment = align.center,
     backgroundColor = colors.red,
-    width = 10,
-    height = 3,
-    padding = 1,
+    width = 20,
+    height = 10,
+    padding = 2,
 }
 
 text:draw()
